a50e9822ac8553092b2e1e4918c36abd0e7e9454
david <david@debian.(none)>
2013-05-20 14:12:03 +0000

    sort_insert ok.

--- sorting/sort_insert.c
+++ sorting/sort_insert.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
+typedef int bool;
+#define true 1
+#define false 0
 
-int insert(int* array, unsigned int size)
+int insert(int* array, unsigned int size, bool ascending)
 {
 	unsigned int i, j;
-	int temp;
+	int min, max;
+
+	if (size <= 1)
+	{
+		printf("sort error: no enought number.\n");
+		return -1;
+	}
 
 	for (i = 0; i < size; i++)
-		for (j = i - 1; j >= 0; j--)
-		{
-			temp = array[i];
-			if (temp < array[j])
+	{
+		if (ascending) {
+			for (j = i; j > 0; j--)
 			{
-				array[i] = array[j];
-				array[j] = temp;
+				min = array[j];
+				if (min < array[j-1])
+				{
+					array[j] = array[j-1];
+					array[j-1] = min;
+				}
+			}
+		} else {
+			for (j = i; j > 0; j--)
+			{
+				max = array[j];
+				if (max > array[j-1])
+				{
+					array[j] = array[j-1];
+					array[j-1] = max;
+				}
 			}
 		}
+	}
 
 	return 0;
 }
@@ -23,9 +46,9 @@ int main(int argc, const char *argv[])
 {
 	int a[5] = {3, 2, 5, 0, 1};
 
-	insert(a, sizeof(a)/sizeof(a[0]));
-
-	printf("%u\n", sizeof(a)/sizeof(a[0]));
+	insert(a, sizeof(a)/sizeof(a[0]), true);
+	printf("%d, %d, %d, %d, %d\n", a[0], a[1], a[2], a[3], a[4]);
+	insert(a, sizeof(a)/sizeof(a[0]), false);
 	printf("%d, %d, %d, %d, %d\n", a[0], a[1], a[2], a[3], a[4]);
 	
 	return 0;
